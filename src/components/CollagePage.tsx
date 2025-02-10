c6e2a3e286c12e52fb28e793ca11b2ca542cd71e
HououinKyouma555 <130073365+HououinKyouma555@users.noreply.github.com>
2025-02-10 11:16:42 -0500

    removed old code

--- src/components/CollagePage.tsx
+++ src/components/CollagePage.tsx
@@ -59,36 +59,6 @@ const CollagePage: React.FC = () => {
 
         {rows}
 
-        <CollagePicture 
-          background={{ background: 'linear-gradient(to bottom, red, orange)' }}
-          image= {data.Pictures[0].image}
-          textOrder = {{ order: 3}}
-          imageOrder = {{ order: 4}}
-          title = {data.Pictures[0].title}
-          date = {data.Pictures[0].date}
-          text = {data.Pictures[0].text}
-        />
-
-        <CollagePicture 
-          background={{ background: 'linear-gradient(to bottom, orange, yellow)' }}
-          image= {data.Pictures[1].image}
-          textOrder = {{ order: 4}}
-          imageOrder = {{ order: 3}}
-          title = {data.Pictures[1].title}
-          date = {data.Pictures[1].date}
-          text = {data.Pictures[1].text}
-        />
-
-        <CollagePicture
-          background={{ background: 'linear-gradient(to bottom, yellow, lightgreen)' }}
-          image="/images/carmirror.jpg"
-          textOrder = {{ order: 4}}
-          imageOrder = {{ order: 3}}
-          title = {"Car Mirror Selfie"}
-          date = {"June 24'"}
-          text = "/text/carmirror.txt"
-        />
-
         <br />
 
         <a href="/" className="back-link">Back to Letter</a>
