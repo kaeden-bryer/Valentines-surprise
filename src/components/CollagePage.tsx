26807b0ab88b4c838f675753152bc598b2ae9b1e
HououinKyouma555 <130073365+HououinKyouma555@users.noreply.github.com>
2025-02-10 11:13:41 -0500

    figured out how to push an array based on the json

--- src/components/CollagePage.tsx
+++ src/components/CollagePage.tsx
@@ -6,14 +6,58 @@ interface data {
   background: string;
 }
 
+interface Picture {
+  image: string;
+  title: string;
+  date: string;
+  text: string;
+}
+
 
 const CollagePage: React.FC = () => {
   
+  const rows: JSX.Element[] = [];
+
+  let i = 0;
+
+  data.Pictures.forEach((picture: Picture) => {
+
+    let textOrder: React.CSSProperties;
+    let imageOrder: React.CSSProperties;
+
+
+    if (i % 2 === 0) {
+      // add textOrder = 3, imageOrder = 4
+      textOrder = { order: 3};
+      imageOrder = { order: 4};
+    } else {
+      // add textOrder = 4, imageOrder = 3
+      textOrder = { order: 4};
+      imageOrder = { order: 3};
+    }
+
+    rows.push(
+      <CollagePicture 
+        image= {picture.image}
+        title = {picture.title}
+        textOrder = {textOrder}
+        imageOrder = {imageOrder}
+        date = {picture.date}
+        text = {picture.text}
+      />
+    );
+    console.log("adding picture");
+    i++;
+});
+
+
   return (
     <>
       <div className="collage-page">
 
         <h1>💖Our Memories✨</h1>
+
+        {rows}
 
         <CollagePicture 
           background={{ background: 'linear-gradient(to bottom, red, orange)' }}
