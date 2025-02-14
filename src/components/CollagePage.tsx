6248179aefca3d1f2e7e4932f0e2a4b282bb3241
HououinKyouma555 <130073365+HououinKyouma555@users.noreply.github.com>
2025-02-14 03:35:19 -0500

    added all text and image files

--- src/components/CollagePage.tsx
+++ src/components/CollagePage.tsx
@@ -21,13 +21,37 @@ const CollagePage: React.FC = () => {
   const rows: JSX.Element[] = [];
 
   let i = 0;
+  let colorIndex = 0;
 
   data.Pictures.forEach((picture: Picture) => {
 
     let textOrder: React.CSSProperties;
     let imageOrder: React.CSSProperties;
     let width: React.CSSProperties;
+    let background: React.CSSProperties = { background: "linear-gradient(to bottom, red, orange)" };
 
+    if (colorIndex == 0){
+      background = {background: "linear-gradient(to bottom, red, orange"};
+      colorIndex++;
+    } else if (colorIndex == 1){
+      background = {background: "linear-gradient(to bottom, orange, yellow"};
+      colorIndex++;
+    } else if (colorIndex == 2){
+      background = {background: "linear-gradient(to bottom, yellow, lightgreen"}; 
+      colorIndex++;
+    } else if (colorIndex == 3){ 
+      background = {background: "linear-gradient(to bottom, lightgreen, lightblue"};
+      colorIndex++;
+    } else if (colorIndex == 4){
+      background = {background: "linear-gradient(to bottom, lightblue, blue"};
+      colorIndex++;
+    } else if (colorIndex == 5){
+      background = {background: "linear-gradient(to bottom, blue, purple"};
+      colorIndex++;
+    } else if (colorIndex == 6){
+      background = {background: "linear-gradient(to bottom, purple, red"};
+      colorIndex = 0;
+    }
 
     if (i % 2 === 0) {
       // add textOrder = 3, imageOrder = 4
@@ -54,6 +78,7 @@ const CollagePage: React.FC = () => {
         date = {picture.date}
         text = {picture.text}
         orientation = {width}
+        background = {background}
       />
     );
     console.log("adding picture");
@@ -69,7 +94,15 @@ const CollagePage: React.FC = () => {
 
         {rows}
 
-        <br />
+        < Milestone text="2023" />
+
+        < Milestone text="Officially Dating" />
+
+        < Milestone text="2024" />
+
+        < Milestone text="At Oakland Together" />
+
+        < Milestone text="2025" />
 
         < Milestone text="Present Date" />
 
